ef28d0c8eb5529f395fe013810b0687b6e568697
vthlea <l.voith@s-bleyer-gmbh.de>
2026-07-07 11:59:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
+const CACHE_NAME = "inspektionsprotokoll-v1";
 
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./Farbstreifen.pdf",
+  "./libs/html2canvas.min.js",
+  "./libs/jspdf.umd.min.js",
+  "./libs/pdf.min.js",
+  "./libs/pdf.worker.min.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
